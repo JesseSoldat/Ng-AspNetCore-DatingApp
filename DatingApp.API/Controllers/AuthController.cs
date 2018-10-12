344312dd8c214ff5831f9ab618d6c293e255c885
JLab <jessesoldat@gmail.com>
2018-10-12 07:45:33 -0500

    global exception handler for prod on server

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,6 +45,7 @@ namespace DatingApp.API.Controllers
     [HttpPost("login")]
     public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
     {
+      throw new Exception("no you didn't");
       var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
       if (userFromRepo == null) return Unauthorized();
