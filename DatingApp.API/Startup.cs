c3554ab69f3f5e86c4f6f0526aa69dd1d3a4cb10
JLab <jessesoldat@gmail.com>
2018-10-16 20:25:30 -0500

    users repo and users controller

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,8 +37,16 @@ namespace DatingApp.API
       services.AddCors();
       services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
       services.AddTransient<Seed>();
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+      .AddJsonOptions(opt =>
+      {
+        opt.SerializerSettings.ReferenceLoopHandling =
+            Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+      });
+
       services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddScoped<IDatingRepository, DatingRepository>();
       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
         {
@@ -82,7 +90,7 @@ namespace DatingApp.API
       }
 
       // app.UseHttpsRedirection();
-      seeder.SeedUsers();
+      // seeder.SeedUsers();
       app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
       app.UseAuthentication();
       app.UseMvc();
